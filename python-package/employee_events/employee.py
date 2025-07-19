a53b11474356163081682ef97a901df002f7ee74
Pote Pongchaikul <potepongchaikul@gmail.com>
2025-07-20 00:08:40 +0700

    Create 'Employee' class

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,17 +1,24 @@
 # Import the QueryBase class
 #### YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 #### YOUR CODE HERE
+from sqlite3 import connect
+from pathlib import Path
+from functools import wraps
+import pandas as pd
 
 # Define a subclass of QueryBase
 # called Employee
 #### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
     #### YOUR CODE HERE
+    name = "employee"
 
 
     # Define a method called `names`
@@ -19,6 +26,7 @@
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
+    def names(self):
         
         # Query 3
         # Write an SQL query
@@ -28,6 +36,16 @@
         # This query should return the data
         # for all employees in the database
         #### YOUR CODE HERE
+        query_string = f"""
+            select 
+                concat(first_name, " ",last_name) as full_name, 
+                {self.name}_id
+            from
+                {self.name}
+            """
+        
+        results = super().query(query_string)
+        return results
     
 
     # Define a method called `username`
@@ -35,6 +53,7 @@
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -43,6 +62,16 @@
         # to only return the full name of the employee
         # with an id equal to the id argument
         #### YOUR CODE HERE
+        query_string = f""" 
+            select
+                concat(first_name, " ",last_name) as full_name
+            from
+                {self.name}
+            where
+                {self.name}.{self.name}_id = {id}
+            """
+        results = super().query(query_string)
+        return results
 
 
     # Below is method with an SQL query
@@ -55,7 +84,15 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        # return f"""
+        #             SELECT SUM(positive_events) positive_events
+        #                  , SUM(negative_events) negative_events
+        #             FROM {self.name}
+        #             JOIN employee_events
+        #                 USING({self.name}_id)
+        #             WHERE {self.name}.{self.name}_id = {id}
+        #         """
+        query_string = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +100,6 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+
+        results = super().pandas_query(query_string)
+        return results
