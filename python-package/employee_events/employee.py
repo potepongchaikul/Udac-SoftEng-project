3d4471026e1eab5db715b35ff7d38382e4cfb8ca
Pote Pongchaikul <potepongchaikul@gmail.com>
2025-07-20 16:42:15 +0700

    Remove prompts: #### YOUR CODE HERE

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,10 +1,8 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
 from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
 from sqlite3 import connect
 from pathlib import Path
 from functools import wraps
@@ -12,30 +10,25 @@ import pandas as pd
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
 class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
     name = "employee"
-
-
+    
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
     def names(self):
         
         # Query 3
         # Write an SQL query
         # that selects two columns 
-        # 1. The employee's full name
-        # 2. The employee's id
+        #   1. The employee's full name
+        #   2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
         query_string = f"""
             select 
                 concat(first_name, " ",last_name) as full_name, 
@@ -47,12 +40,10 @@ class Employee(QueryBase):
         results = super().query(query_string)
         return results
     
-
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
     def username(self, id):
         
         # Query 4
@@ -61,7 +52,6 @@ class Employee(QueryBase):
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
         query_string = f""" 
             select
                 concat(first_name, " ",last_name) as full_name
@@ -81,17 +71,7 @@ class Employee(QueryBase):
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
-
-        # return f"""
-        #             SELECT SUM(positive_events) positive_events
-        #                  , SUM(negative_events) negative_events
-        #             FROM {self.name}
-        #             JOIN employee_events
-        #                 USING({self.name}_id)
-        #             WHERE {self.name}.{self.name}_id = {id}
-        #         """
         query_string = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
